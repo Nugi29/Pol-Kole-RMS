fb6859ff9ce0a725609abf2b971476b091671009
Nugi29 <nugitha.c@gmail.com>
2026-09-05 02:45:01 +0530

    deployment: add environment configurations

--- Pol-Kole-Frontend/src/environments/environment.ts
+++ Pol-Kole-Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://44.223.27.62:8080/api',
-  wsUrl: 'ws://44.223.27.62:8080/ws/orders',
+  apiUrl: 'http://ec2-44-223-27-62.compute-1.amazonaws.com:8080/api',
+  wsUrl: 'ws://ec2-44-223-27-62.compute-1.amazonaws.com:8080/ws/orders',
   loginBgImageUrl: 'assets/polkolelogo.png',
 };
